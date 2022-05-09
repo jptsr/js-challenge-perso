ea9e00a45db81e11c265d41b12fdc799b745f098
Julie <juptsr.becode@gmail.com>
2022-05-09 19:24:53 +0200

    ajout de composants monofichiers

---
+++ rooter-vuejs/cli-version/src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app" id="app">
+			<h1>La belle vue</h1>
+			<p class="description">
+				Bienvenue dans notre café la belle vue ! Nous sommes réputés pour notre
+				pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le matin
+				ou avec un goûter réconfortant. Mais attention, vous verrez qu'il est
+				difficile de s'arrêter.
+			</p>
+			<footer class="footer">
+				<p>Copyright La belle vue 2020</p>
+			</footer>
+		</div>
+</template>
+
+<script>
+    export default ({
+        name: 'HomePage'
+    })
+</script>
---
+++ rooter-vuejs/cli-version/src/components/MenuItem.vue
@@ -0,0 +1,38 @@
+<script>
+    export default ({
+        name: 'MenuItem',
+        props: [ ]
+    })
+</script>
+
+<template>
+    <section class="menu">
+		<h2>Menu</h2>
+
+		<div v-for="item in simpleMenu" :key="item.name" class="menu-item">
+			<img
+				class="menu-item__image"
+				:src="item.image.source"
+				:alt="item.image.alt"
+			/>
+			<div>
+				<h3>{{ item.name }}</h3>
+				<p v-if="item.inStock">En stock</p>
+				<p v-else>En rupture de stock</p>
+				<div>
+					<label for="add-item-quantity"
+						>Quantité : {{ item.quantity }}</label
+					>
+					<input
+						v-model.number="item.quantity"
+						id="add-item-quantity"
+						type="number"
+					/>
+					<button @click="addToShoppingCart(item.quantity)">
+					Ajouter au panier d'achat
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
